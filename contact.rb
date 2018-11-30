e8362d6eabebb60dfc7ba1ed3730c86db0489c22
victoriak94 <vkamila6@gmail.com>
2018-11-30 15:48:35 -0500

    Added elements to css stylesheet and views pages for contacts and layout.

--- contact.rb
+++ contact.rb
@@ -20,11 +20,38 @@ end
 
 Contact.auto_upgrade!
 
+at_exit do
+  ActiveRecord::Base.connection.close
+end
+
+Contact.destroy_all
+
 p victoria = Contact.create(
   first_name: "Victoria",
   last_name:  "Kamila",
   email:      "vkamila6@gmail.com",
   note:       "Is a libra sun, taurus moon, and libra rising."
+)
+
+p william = Contact.create(
+  first_name: "William",
+  last_name:  "Selviz",
+  email:      "willselviz@gmail.com",
+  note:       "Virgo king."
+)
+
+p marina = Contact.create(
+  first_name: "Marina",
+  last_name:  "Sinko",
+  email:      "marinasinko@gmail.com",
+  note:       "libra rising GANGGGG."
+)
+
+p gabi = Contact.create(
+  first_name: "Gabriella",
+  last_name:  "Cuello",
+  email:      "gabi@gmail.com",
+  note:       "aint nothin like the libra/sag bond."
 )
 
 puts victoria.inspect
--- crm.rb
+++ crm.rb
@@ -4,15 +4,29 @@ require_relative 'contact'
 require 'sinatra'
 
 get '/home' do
+  @title = "Bitmaker CRM"
   erb :index
 end
 
 get '/contacts' do
+  @title = "Bitmaker CRM"
   @contacts = Contact.all
   erb :contacts
 end
 
+get '/contacts/:id' do
+  # @title = @contact.first_name + "'s Info"
+  @contact = Contact.find_by({id: params[:id].to_i})
+  if @contact
+    erb :show_contact
+  else
+    raise Sinatra::NotFound
+  end
+  erb :show_contact
+end
+
 get '/about_me' do
+  @title = "About"
   erb :about_me
 end
 
