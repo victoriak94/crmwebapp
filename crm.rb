36ecfaf43824894d5b5478e7cf5c58ca43de8c99
victoriak94 <vkamila6@gmail.com>
2018-12-02 22:06:52 -0500

    Added delete contact option. Added links on individual contact page and edit contact page to navigate between each other.

--- crm.rb
+++ crm.rb
@@ -58,6 +58,32 @@ get '/contacts/:id/edit' do
   erb :edit_contact
 end
 
+put '/contacts/:id' do
+  @contact = Contact.find_by(id: params[:id].to_i)
+  if @contact
+    @contact.update(
+    first_name: params[:first_name],
+    last_name:  params[:last_name],
+    email:      params[:email],
+    note:       params[:note]
+    )
+
+    redirect to('/contacts')
+  else
+    raise Sinatra::NotFound
+  end
+end
+
+delete '/contacts/:id' do
+  @contact = Contact.find_by(params[:id].to_i)
+  if @contact
+    @contact.delete
+    redirect to('/contacts')
+  else
+    raise Sinatra::NotFound
+  end
+end
+
 after do
   ActiveRecord::Base.connection.close
 end
